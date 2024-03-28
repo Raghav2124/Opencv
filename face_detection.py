a057479fd68653a1742a3505f8e5469dec435e23
Raghav2124 <raghav.aggarwal@testingxperts.com>
2024-03-28 18:34:43 +0530

    adding files

--- face_detection.py
+++ face_detection.py
@@ -1,6 +1,16 @@
 import cv2 as cv 
-img=cv.imread("Photos/lady.jpg")
+img=cv.imread("license.jpeg")
 cv.imshow('Lady',img)
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray",gray)
+haer_=cv.CascadeClassifier('haer_faces.xml')
+faces_rect=haer_.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+print(f'Number of faces found={len(faces_rect)}')
+
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Faces', img)
+
 cv.waitKey(0)
